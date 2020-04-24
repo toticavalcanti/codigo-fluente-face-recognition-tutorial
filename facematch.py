19193db8479921eaf151f03131bab8f3763a1af0
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-04-24 13:37:09 -0300

    first commit

---
+++ facematch.py
@@ -0,0 +1,19 @@
+import face_recognition
+
+
+fernanda_image = face_recognition.load_image_file('./img/known/fernanda_montenegro.jpg')
+# face_encodings encode an image at 128 dimentions
+fernanda_face_encoding = face_recognition.face_encodings(fernanda_image)[0]
+
+unknown_image = face_recognition.load_image_file('./img/unknown/fernanda-600x398.jpg')
+
+unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+# Compare faces
+results = face_recognition.compare_faces(
+    [bill_face_encoding], unknown_face_encoding)
+
+if results[0]:
+    print('Essa é a Fernanda Montenegro')
+else:
+    print('Essa NÃO é a Fernanda Montenegro')
