3c61585f80333a812fc6c73792b14f0079427a39
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-04-27 22:23:03 -0300

    change facematch.py to faceMatch.py (camel case)

--- facematch.py
+++ facematch.py
@@ -1,11 +1,11 @@
 import face_recognition
 
 
-fernanda_image = face_recognition.load_image_file('./img/known/fernanda_montenegro.jpg')
+fernanda_image = face_recognition.load_image_file('./img/known/faustao.jpg')
 # face_encodings encode an image at 128 dimentions
 fernanda_face_encoding = face_recognition.face_encodings(fernanda_image)[0]
 
-unknown_image = face_recognition.load_image_file('./img/unknown/fernanda-600x398.jpg')
+unknown_image = face_recognition.load_image_file('./img/unknown/15843071585e6e9bd65826e_1584307158_3x2_lg.jpg')
 
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
@@ -14,6 +14,6 @@ results = face_recognition.compare_faces(
     [fernanda_face_encoding], unknown_face_encoding)
 
 if results[0]:
-    print('Essa é a Fernanda Montenegro')
+    print('Essa é o Fausto')
 else:
-    print('Essa NÃO é a Fernanda Montenegro')
+    print('Essa NÃO é o Fausto')
