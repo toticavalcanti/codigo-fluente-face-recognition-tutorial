a2d6feac7b7140e5de519000290d81fdd855cc45
Toti Cavalcanti <toticavalcanti@hotmail.com>
2020-04-30 00:30:13 -0300

    upload pullfaces.py and identify.py

--- identify.py
+++ identify.py
@@ -10,21 +10,26 @@ faustao_face_encoding = face_recognition.face_encodings(image_of_faustao)[0]
 image_of_xuxa = face_recognition.load_image_file('./img/known/xuxa.jpg')
 xuxa_face_encoding = face_recognition.face_encodings(image_of_xuxa)[0]
 
+image_of_reinaldo = face_recognition.load_image_file('./img/known/reinaldo.jpg')
+reinaldo_face_encoding = face_recognition.face_encodings(image_of_reinaldo)[0]
+
 #  Create arrays of encodings and names
 known_face_encodings = [
   silvio_santos_face_encoding,
   faustao_face_encoding,
-  xuxa_face_encoding
+  xuxa_face_encoding,
+  reinaldo_face_encoding
 ]
 
 known_face_names = [
   "Silvio Santos",
   "Fausto Silva",
-  "Xuxa"
+  "Xuxa",
+  "Reynaldo Gianecchini"
 ]
 
 # Load test image to find faces in
-test_image = face_recognition.load_image_file('./img/groups/xuxa_fausto_silvio.jpg')
+test_image = face_recognition.load_image_file('./img/groups/xuxa_fausto_silvio_reinaldo.jpg')
 
 # Find faces in test image
 face_locations = face_recognition.face_locations(test_image)
@@ -39,9 +44,8 @@ draw = ImageDraw.Draw(pil_image)
 # Loop through faces in test image
 for(top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
   matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
   name = "Unknown Person"
-
+ 
   # If match
   if True in matches:
     first_match_index = matches.index(True)
